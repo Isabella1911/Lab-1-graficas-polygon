b1b1949589e1283fdcec3121e06a7d9c5594e56d
root <root@Isabella>
2025-07-09 22:52:33 -0600

    Dibuja Poligon-1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use tiny_skia::*;
 fn main() {
     let mut pixmap = Pixmap::new(800, 500).unwrap();
 
-    //draw_polygon1(&mut pixmap);
+    draw_polygon1(&mut pixmap);
     //draw_polygon2(&mut pixmap);
     //draw_polygon3(&mut pixmap);
     //draw_polygon4(&mut pixmap);
